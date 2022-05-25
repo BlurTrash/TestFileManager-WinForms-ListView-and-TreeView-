5291c850d8fee6d3ecf14ad56c2721c5177f16b3
Pavel Kuvshinchikov <kuvshinchikov7127@mail.ru>
2022-05-25 21:55:12 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageListTreeView = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.backPTextBox = new System.Windows.Forms.TextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.видToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.currentPTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageListTreeView;
+            this.treeView1.Location = new System.Drawing.Point(13, 242);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(284, 418);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDoubleClick);
+            // 
+            // imageListTreeView
+            // 
+            this.imageListTreeView.ColorDepth = System.Windows.Forms.ColorDepth.Depth16Bit;
+            this.imageListTreeView.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListTreeView.ImageStream")));
+            this.imageListTreeView.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListTreeView.Images.SetKeyName(0, "localdisc.png");
+            this.imageListTreeView.Images.SetKeyName(1, "folder.png");
+            this.imageListTreeView.Images.SetKeyName(2, "file.png");
+            this.imageListTreeView.Images.SetKeyName(3, "image.png");
+            this.imageListTreeView.Images.SetKeyName(4, "text.png");
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList2;
+            this.listView1.Location = new System.Drawing.Point(303, 242);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(793, 418);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "localdisc.png");
+            this.imageList2.Images.SetKeyName(1, "folder.png");
+            this.imageList2.Images.SetKeyName(2, "file.png");
+            this.imageList2.Images.SetKeyName(3, "image.png");
+            this.imageList2.Images.SetKeyName(4, "text.png");
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "localdisc.png");
+            this.imageList1.Images.SetKeyName(1, "folder.png");
+            this.imageList1.Images.SetKeyName(2, "file.png");
+            this.imageList1.Images.SetKeyName(3, "image.png");
+            this.imageList1.Images.SetKeyName(4, "text.png");
+            // 
+            // backPTextBox
+            // 
+            this.backPTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.backPTextBox.Location = new System.Drawing.Point(303, 42);
+            this.backPTextBox.Name = "backPTextBox";
+            this.backPTextBox.Size = new System.Drawing.Size(793, 27);
+            this.backPTextBox.TabIndex = 2;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 38);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(127, 34);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "<- Назад";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(170, 38);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(127, 34);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "Вперед ->";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.видToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1108, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // видToolStripMenuItem
+            // 
+            this.видToolStripMenuItem.Name = "видToolStripMenuItem";
+            this.видToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.видToolStripMenuItem.Text = "Вид";
+            // 
+            // currentPTextBox
+            // 
+            this.currentPTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.currentPTextBox.Location = new System.Drawing.Point(303, 100);
+            this.currentPTextBox.Name = "currentPTextBox";
+            this.currentPTextBox.Size = new System.Drawing.Size(793, 27);
+            this.currentPTextBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(303, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "BackPath";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "FPath";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(303, 153);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(793, 27);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(303, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "NextPath";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 672);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.currentPTextBox);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.backPTextBox);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox backPTextBox;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem видToolStripMenuItem;
+        private System.Windows.Forms.ImageList imageListTreeView;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox currentPTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string pathTreeView = "";
+        string backPath; //предыдущий путь
+        string currentPath; //текущий путь
+        string nextPath; //следующий путь
+        List<string> listPathDirs = new List<string>(); //список путей директорий 
+        Stack<string> nextPathStack = new Stack<string>(); //стек для следующих путей при нажатии кнопки вперед, если они есть
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Мой копьютер";
+        }
+
+        //Листвью
+        void GetLocalDir() //Получить логические диски
+        {
+            try
+            {
+                listView1.Items.Clear();
+                string[] directories = Environment.GetLogicalDrives();
+                foreach (string item in directories)
+                {                   
+                    //для listview
+                    listView1.Items.Add(item, 0);
+                    listPathDirs.Add(item);
+                }
+                btnBack.Enabled = false;                
+                backPath = "";
+                backPTextBox.Text = backPath;
+                currentPath = "Мой компьютер";
+                currentPTextBox.Text = currentPath; 
+                if (nextPathStack.Count != 0)
+                {
+                    nextPath = nextPathStack.Peek();
+                    btnNext.Enabled = true;
+                }
+                textBox3.Text = nextPath; 
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+        }
+        void GetFiles() //Получить файлы и директории
+        {
+
+            listView1.BeginUpdate();
+            try
+            {
+                btnBack.Enabled = true; 
+
+                var dirs = Directory.GetDirectories(currentPath) //фильтр на директории - убираем скрытые и системные папки
+                    .Select(d => new { Attr = new DirectoryInfo(d).Attributes, Dir = d })
+                    .Where(x => !x.Attr.HasFlag(FileAttributes.System))
+                    .Where(x => !x.Attr.HasFlag(FileAttributes.Hidden))
+                    .Select(x => x.Dir)
+                    .ToList();
+
+                listPathDirs.Clear(); //очищаем список директорий
+
+                try //получаем путь родительского каталога если он есть
+                {
+                    DirectoryInfo dInfo = new DirectoryInfo(currentPath);
+                    backPath = dInfo.Parent?.FullName ?? "Мой компьютер";
+                    backPTextBox.Text = backPath;
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+                listView1.Items.Clear(); //очищаем листвью и начинаем его перезаполнять
+                foreach (string s in dirs) //добавляем каталоги из фильтра в листвью и в список путей директорий
+                {
+                    string dirName = System.IO.Path.GetFileName(s);
+                    listView1.Items.Add(dirName, 1);
+                    listPathDirs.Add(s);
+                }
+                string[] files = Directory.GetFiles(currentPath); //получаем и добавляем файлы в листвью в текущей директории
+                foreach (string s in files)
+                {
+                    string fileName = System.IO.Path.GetFileName(s);
+                    if (Path.GetExtension(fileName) == ".png" || Path.GetExtension(fileName) == ".jpeg" || Path.GetExtension(fileName) == ".bmp" || Path.GetExtension(fileName) == ".ico")
+                    {
+                        listView1.Items.Add(fileName, 3);
+                    }
+                    else if (Path.GetExtension(fileName) == ".txt")
+                    {
+                        listView1.Items.Add(fileName, 4);
+                    }
+                    else
+                    {
+                        listView1.Items.Add(fileName, 2);
+                    }
+                }
+
+                //Перезаписываем пути для тексбоксов
+                currentPTextBox.Text = currentPath; 
+                if (nextPathStack.Count != 0) //если стек не пустой
+                {
+                    textBox3.Text = nextPathStack.Peek();
+                    nextPath = nextPathStack.Peek();
+                }
+                else
+                {
+                    textBox3.Text = ""; 
+                    btnNext.Enabled = false;
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+            listView1.EndUpdate();
+        }
+
+
+        //Тривью
+        public void fillNode(string path, TreeNode parentNode)
+        {
+            var dirs = Directory.GetDirectories(path)
+                    .Select(d => new { Attr = new DirectoryInfo(d).Attributes, Dir = d })
+                    .Where(x => !x.Attr.HasFlag(FileAttributes.System))
+                    .Where(x => !x.Attr.HasFlag(FileAttributes.Hidden))
+                    .Select(x => x.Dir)
+                    .ToList();
+            try
+            {
+                foreach (string dir in dirs) //заполнение узлов каталогами
+                {
+                    //c картинками привязываем                    
+                    TreeNode node = new TreeNode(System.IO.Path.GetFileName(dir), 1, 1);
+                    node.Name = dir; 
+                    parentNode.Nodes.Add(node);
+                    fillNode(dir, node); //рекурсионный обход каталогов
+                }
+                //foreach (string file in dirs) //Заполнение узлов файлами - отключил, много файлов к которым нет доступа
+                //{
+                //    //определяем расширение файла и привязываем картинки
+                //    string fileName = System.IO.Path.GetFileName(file);
+                //    if (Path.GetExtension(fileName) == ".png" || Path.GetExtension(fileName) == ".jpeg" || Path.GetExtension(fileName) == ".bmp" || Path.GetExtension(fileName) == ".ico")
+                //        parentNode.Nodes.Add(file, file, 3);
+                //    else if (Path.GetExtension(fileName) == ".txt")
+                //        parentNode.Nodes.Add(file, file, 4);
+                //    else
+                //        parentNode.Nodes.Add(file, file, 2);
+                //}
+            }
+            catch { } // Если обрабатывать исключения, выдает ошибку "access to the path is denied"
+            //штук 8 папок к которым нет доступа пишет, у меня на ноуте один логический диск C:
+            //так и не нашел решения(( как понял система в целях безопасности не дает доступ к катим то системным файлам
+            //catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        
+
+        //События
+        private void Form1_Load(object sender, EventArgs e) 
+        {
+            btnNext.Enabled = false;
+            GetLocalDir();
+
+            string[] directories = Environment.GetLogicalDrives();
+            foreach (string item in directories)
+            {
+                //для treeview собираем дерево
+                TreeNode nodeLD = new TreeNode(item, 0, 0);
+                nodeLD.Name = item;
+                treeView1.Nodes.Add(nodeLD);
+                fillNode(item, nodeLD);
+            }
+        }
+        private void listView1_ItemActivate(object sender, EventArgs e) //активация итема
+        {
+            if (listView1.SelectedItems.Count == 0)
+                return;
+
+            ListViewItem item = listView1.SelectedItems[0];
+            if (item.ImageIndex == 1 || item.ImageIndex == 0)
+            {
+                string itemName = item.Text; 
+                string title = "";
+                
+                foreach (string path in listPathDirs) //ищем активированную директорию
+                {
+                    string dirName = path.Split('\\').LastOrDefault(); //обрезаем каждый путь чтобы получить имя директории
+                    try
+                    {
+                        if (path == itemName || dirName == itemName) //path.Substring(path.Length - it.Length, it.Length) == it
+                        {
+                            currentPath = path;
+                            title = path;
+                        }
+                    }
+                    catch(Exception ex) { MessageBox.Show(ex.Message); }
+                }
+                try
+                {
+                    this.Text = title;
+                    nextPathStack.Clear();
+                    btnNext.Enabled = false;
+                    GetFiles();
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+            }
+            else
+            {
+                string start = this.Text + "\\" + item.Text;
+                System.Diagnostics.Process.Start(new ProcessStartInfo(start) { UseShellExecute = true });
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e) //Кнопка переход назад
+        {
+            bool b = false;
+            String[] LogicalDrives = Environment.GetLogicalDrives();
+            foreach (string s in LogicalDrives)
+            {
+                if (backPath == s && currentPath == s) b = true; 
+            }
+            if ((backPath != null && backPath != "Мой компьютер") && !b) 
+            {
+                this.Text = backPath;
+                nextPath = currentPath;
+                currentPath = backPath;
+                btnNext.Enabled = true;
+                nextPathStack.Push(nextPath); // при нажатии кнопки назад заносим путь из которого вышли в стек                                                    
+                GetFiles();
+            }
+            else //в ином случае вернулись в начало 
+            {
+                nextPathStack.Push(currentPath);
+                btnBack.Enabled = false;
+                GetLocalDir();
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e) //кнопка перехода вперед
+        {
+            try
+            {
+                if (nextPath != null && nextPathStack.Count != 0)
+                {
+                    backPath = currentPath;
+                    currentPath = nextPathStack.Pop();
+                    GetFiles();
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            pathTreeView = treeView1.SelectedNode.Name;
+            currentPath = pathTreeView;
+            nextPathStack.Clear();
+            GetFiles();          
+            nextPath = "";
+        }
+    }
+}
